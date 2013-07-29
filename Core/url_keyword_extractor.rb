9fda6143f30db2cd8aefe450958f2ccc9db8827d
Jeremy Owen <me@jeremykowen.com>
2013-07-29 01:56:12 -0500

    Initial Commit
    Got unit test runner working and a basic test suite.

---
+++ Core/url_keyword_extractor.rb
@@ -0,0 +1,7 @@
+class UrlKeywordExtractor
+  def extract url
+    t1 = Thread.new {sleep 0.5}
+    t1.join
+    return {}
+  end
+end
---
+++ UnitTests/urlKeywordExtractor_test.rb
@@ -0,0 +1,66 @@
+require 'test/unit'
+require 'benchmark'
+require_relative '../Core/url_keyword_extractor'
+
+
+class UrlKeywordExtractorTests < Test::Unit::TestCase
+  #Test URLS
+  SIMPLE_TEST_URL ='https://www.google.com/search?q=What+is+the+weather+like+on+mars&aqs=chrome.0.69i57j0l3j69i62l2.10643j0&sourceid=chrome&ie=UTF-8'
+  CASE_ONLY_TEST_URL = 'https://www.google.com/search?q=WhatIsTheWeatherLikeOnMars&aqs=chrome.0.69i57j0l3j69i62l2.10643j0&sourceid=chrome&ie=UTF-8'
+  DIFFICULT_TEST_URL = 'https://www.google.com/search?q=whatistheweatherlikeonmars&aqs=chrome.0.69i57j0l3j69i62l2.10643j0&sourceid=chrome&ie=UTF-8'
+
+  #Expected Test Results
+  EXPECTED = {keywords:%w(what is the weather like on mars)}
+
+  attr_accessor :extractor
+
+  def setup
+    @extractor =  UrlKeywordExtractor.new
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_extractor_exists
+    assert_not_nil @extractor
+  end
+
+  def test_extractor_returns_hash
+    keywords = @extractor.extract SIMPLE_TEST_URL
+    assert keywords.kind_of?(Hash)
+  end
+
+  def test_correct_for_simple_test
+    result = @extractor.extract(SIMPLE_TEST_URL)
+    assert_equal(EXPECTED[:keywords], result[:keywords])
+  end
+
+  def test_correct_for_case_only_test
+    result = @extractor.extract(CASE_ONLY_TEST_URL)
+    assert_equal(EXPECTED[:keywords], result[:keywords])
+  end
+
+  def test_correct_for_difficult_test
+    result = @extractor.extract(DIFFICULT_TEST_URL)
+    assert_equal(EXPECTED[:keywords], result[:keywords])
+  end
+
+  def test_performant_for_simple_test
+    time = Benchmark.measure {@extractor.extract(SIMPLE_TEST_URL)}
+    assert time.real < 0.01
+  end
+
+  def test_performant_for_case_only_test
+    time = Benchmark.measure {@extractor.extract(CASE_ONLY_TEST_URL)}
+    assert time.real < 0.01
+  end
+
+  def test_performant_for_difficult_test
+    time = Benchmark.measure {@extractor.extract(DIFFICULT_TEST_URL)}
+    assert time.real < 0.01
+  end
+
+
+
+end
